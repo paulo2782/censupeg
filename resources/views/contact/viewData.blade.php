bd85ae2906a66b05e5a0e0b6b335d7a929f191aa
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-10-18 20:21:13 -0300

    alter viewData

--- resources/views/contact/viewData.blade.php
+++ resources/views/contact/viewData.blade.php
@@ -1,66 +1,67 @@
 @extends('layouts.app')
 @include('includes/header')
 <body>
-<div class="card text-center">
-	<div class="card-body">
-		<h4 class="card-title">CONTATO
-		<a href="#"><img src="{{ asset('img/button-edit.png') }}" alt="Botão Editar" id="btnEditContact" title="Editar Contato"></a>
-		</h4>
-		<h6 class="card-subtitle mb-2 text-muted">{{ $dados[0]->name }}</h6>
-	</div>
-</div>
-
-<div class="container-fluid">
-    <div class="card-body">
+<section class="container-main">
+    <div class="content border border-primary">
+      <div class="top-bar">
+	  	<h4 class="mt-4 text-center">{{ $dados[0]->name }}</h4>
+      </div>
+      <div class="line-horizontal"></div>
+      <div class="content-table">
 		<div class="row">
-			<div class="col-lg-12">
+			<div class="col-12">
+				<h5>Dados Pessoais
+					<i class="fa fa-pencil" aria-hidden="true"></i>
+				</h5>
+			</div>
+			<div class="col-12">
 				<label>Nome:</label>
 				<strong>{{ $dados[0]->name }}</strong>
 			</div>
-			<div class="col-lg-12">
-				<label>e-mail:</label>
+			<div class="col-12">
+				<label>E-mail:</label>
 				<strong>{{ $dados[0]->email }}</strong>
 			</div>
-			<div class="col-lg-12">
+			<div class="col-12">
 				<label>Telefone:</label>
 				<strong>{{ $dados[0]->phone }}</strong>
 			</div>
-			<div class="col-lg-12">
+			<div class="col-12">
 				<label>Escolaridade:</label>
 				<strong>{{ $dados[0]->schooling }}</strong>
 			</div>
-			<div class="col-lg-12">
+			<div class="col-12">
 				<label>Estado:</label>
 				<strong>{{ $dados[0]->state }}</strong>
 			</div>
-			<div class="col-lg-12">
+			<div class="col-12">
 				<label>Cidade:</label>
 				<strong>{{ $dados[0]->city }}</strong>
 			</div>
-			<div class="col-lg-12">
+			<div class="col-12">
 				<label>Origem de Contato:</label>
 				<strong>{{ $dados[0]->contact_origin }}</strong>
 			</div>
-			<div class="col-lg-12">
+			<div class="col-12">
 				<label>Informações Adicionais:</label>
 				<strong>{{ $dados[0]->additional_information }}</strong>
 			</div>
-		</div>
-	</div>
-</div>
-
-<div class="card text-center">
-	<div class="card-body">
-		<h4 class="card-title">CURSO
-		<a href="#"><img src="{{ asset('img/button-add.png') }}" alt="Botão adicionar" id="btnAddCourse" title="Adicionar Curso"></a>
-		</h4>
-	</div>
-</div>
-
-<div class="card text-center">
-	<div class="card-body">
-		<h4 class="card-title">LIGAÇÕES
-		<a href="#"><img src="{{ asset('img/button-add.png') }}" alt="Botão adicionar" id="btnAddCall" title="Adicionar Ligação"></a>
-		</h4>
-	</div>
-</div>
+      	</div>
+		<div class="line-horizontal"></div>
+		<div class="row">
+			<div class="col-12">
+				<h5>Cursos
+					<i class="fa fa-pencil" aria-hidden="true"></i>
+				</h5>
+			</div>
+      	</div>
+		  <div class="line-horizontal"></div>
+		<div class="row">
+			<div class="col-12">
+				<h5>Ligações
+					<i class="fa fa-pencil" aria-hidden="true"></i>
+				</h5>
+			</div>
+      	</div>
+    </div>
+</section>

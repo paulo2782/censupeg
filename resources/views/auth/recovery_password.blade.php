6c11e189fe623e0d8c2f88ad65b9f8503c0ae838
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-11 16:39:54 -0300

    modify screen recovery

--- resources/views/auth/recovery_password.blade.php
+++ resources/views/auth/recovery_password.blade.php
@@ -4,22 +4,22 @@
 <div id="login">
     <div class="container">
         <img class="login-img my-4" src="img/logo-censupeg.png" alt="Logo censupeg">
-        <h1 class="my-2 text-center">{{ __('Register') }} </h1>
+        <h1 class="my-2 text-center">Recuperar senha </h1>
         <form method="POST" action="{{ route('register') }}">
             @csrf
             <div class="form-group">
                 <label for="email">{{ __('E-Mail Address') }}</label>
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary form-control">
-                    {{ __('Register') }}
+                    Recuperar senha
                 </button>
+            </div>
+            <div class="mt-2">
+                <div class="d-flex justify-content-center text-4">
+                    Voltar para a página de login?<a onclick="window.location.href='login'" class="ml-2 login-a">Clique aqui</a>
+                </div>
             </div>
         </form>
     </div>

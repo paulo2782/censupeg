cc4969bc9bc9d403e0a71536a9981248883bce30
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-12 14:14:50 -0300

    add content recovery password

---
+++ resources/views/auth/email_recovery_password.blade.php
@@ -0,0 +1,8 @@
+Logo da Censupeg
+Olá Fulano
+Vocë solicitou recuperar sua senha.
+Para sua segurança, não armazenamos a senha de nossos usuários, para acessar novamente o CRM
+com seu login será precisa criar uma nova senha.
+<button>Criar nova senha</button>
+Atenciosamente,
+Equipe DeCarlo Soluções

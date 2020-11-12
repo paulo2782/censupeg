370a5699081e67192e04b2e560d2b8b3f5957a39
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-12 20:03:50 -0300

    message send email

--- resources/views/auth/send.blade.php
+++ resources/views/auth/send.blade.php
@@ -1,4 +1,12 @@
 <h1>CENSUPEG</h1>
 <br>
-<strong><h4>CLIQUE NESSE LINK PARA REDEFINIR SUA SENHA</h4></strong>
- 
+<h1>Olá Fulano</h1>
+<p>
+    Vocë solicitou recuperar sua senha.
+    Para sua segurança, não armazenamos a senha de nossos usuários, para acessar novamente o CRM
+    com seu login será precisa criar uma nova senha.
+</p>
+<button>Criar nova senha</button>
+<p>Atenciosamente, </p>
+
+<p>Equipe DeCarlo Soluções</p>

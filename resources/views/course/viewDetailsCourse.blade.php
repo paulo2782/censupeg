afb6115dcba1889776314e4ac8ad6e2ef0bc1eff
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-17 08:43:35 -0300

    create view data courses

---
+++ resources/views/course/viewDetailsCourse.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+@include('includes/header')
+@include('contact/modalEdit')
+
+@include('contact/modal_contact_courses')
+@include('contact/modal_contact_schedule')
+@include('contact/modal_contact_edit_courses')
+@include('contact/modal_contact_edit_schedule')
+
+<body id="body-container">
+<div id="container-main">
+	<div class="container">
+		<div class="content-details">
+			<div class="top-bar-block">
+				<h3 class="py-4 text-center">{{ $dados[0]->name }}</h3>
+			</div>
+			<div class="show-details-block">
+				<h2>Dados básicos
+					<a href="#" class="fa fa-pencil" aria-hidden="true" id="btnAdd"></a>
+				</h2>
+				<form class="contact-info" method="post">
+					<div class="form-row">
+						<div class="form-group col-12">
+							<label class="text-4" for="nameContact">Curso</label>
+							<input type="text" class="form-control" readonly="readonly" id="nameContact" name="name" value="{{ $dados[0]->name }}" />
+						</div>
+						<div class="form-group col-md-6 col-12">
+							<label class="text-4" for="emailContact">Modalidade</label>
+							<input type="email" class="form-control" readonly="readonly" id="emailContact" name="email" value="{{ $dados[0]->email }}" />
+						</div>
+						<div class="form-group col-md-6 col-12">
+							<label class="text-4" for="phoneContact">Nível</label>
+							<input type="text" class="form-control" readonly="readonly" id="phoneContact" name="phone" value="{{ $dados[0]->phone }}"/>    
+						</div>
+						<div class="form-group col-md-6 col-12">
+							<label class="text-4" for="cityContact">Valor</label>
+							<input type="text" class="form-control" readonly="readonly" id="cityContact" name="city" value="{{ $dados[0]->city }}"/>    
+						</div>
+						<div class="form-group col-md-6 col-12">
+							<label class="text-4" for="schoolContact">Tempo de duração</label>
+							<input type="text" class="form-control" readonly="readonly" id="schoolContact" name="school" value="{{ $dados[0]->schooling }}"/>    
+						</div>
+						<div class="form-group col-md-6 col-12">
+							<label class="text-4" for="originContact">Link Censupeg</label>
+							<input type="text" class="form-control" readonly="readonly" id="originContact" name="origin" value="{{ $dados[0]->contact_origin }}"/>    
+						</div>
+						<div class="form-group col-12">
+							<label class="text-4" for="observation">Informações adicionais</label>
+							<textarea class="form-control" readonly="readonly" id="additional_information" name="additional_information"> {{ $dados[0]->additional_information }}</textarea>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="show-details-block">
+				<h2>Cursos
+					<a href="#" class="fa fa-plus-circle" aria-hidden="true" id="btnAddCourse"> </a>
+				</h2>
+				<div id="content-table" class="table-responsive">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Curso</th>
+								<th>Modalidade</th>
+								<th>Nível</th>
+								<th>Status</th>
+								<th>Ação</th>
+							</tr>
+						</thead>
+						<tbody>
+							@php $i = 1; @endphp
+							@foreach($dataInterests as $data)
+							<tr>				
+								<td> {{ $i }}</td>			
+								<td> {{ $data->course }}</td>
+								<td> {{ $data->course_type }}</td>
+								<td> {{ $data->level_course }}</td>
+ 								<td> {{ $data->status }}</td>
+								<td>
+									<a href="#" class="fa fa-pencil btnEditCourse" aria-hidden="true" title="Editar Registro" id="{{ $data->id }}"></a>
+
+									<a href="{{ route('destroyInterestCourse',$data->id) }}" class="fa fa-trash btnDelete" aria-hidden="true" title="Apagar Registro"></a>
+								</td>
+							</tr>
+							@php $i++; @endphp
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

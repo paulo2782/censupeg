44b3ffdf4f90f7ad7d1c1d65c7eb81a3ef9c9793
Paulo Oliveira <paulo2782@gmail.com>
2020-11-26 15:27:05 -0300

    resolve conflict fix

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -7,9 +7,6 @@
 	<div id="container-main">
 		<div class="container">
 			<div class="content">
-				<div class="top-bar">
-					<h1>Usuários</h1>
-				</div>
 				<div class="aux-bar">
 					<h2>Usuários </h2>
 				</div>
@@ -39,37 +36,3 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>  
 </html>
-
-
-<!--
-	<div id="container-main">
-    <div class="container">
-        <div class="content">
-            <div class="top-bar">
-                <h1>Usuários</h1>
-            </div>
-            <div id="content-table" class="table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
- 							<th>Nome</th>
- 							<th>Usuário</th>
- 							<th>Permissão</th>
- 						</tr>
-                   </thead>  
-                    <tbody id="tabela">
-                    	@foreach($dados as $dado)
- 						<tr>
- 							<td> {{ $dado->name }} </td>
- 							<td> {{ $dado->email }} </td>
- 							<td> </td>
- 						</tr>
- 						@endforeach
- 					</tbody>
- 				</table>
- 			</div>
- 		</div>
- 	</div>
- </div>
-
--->

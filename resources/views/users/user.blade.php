e02da253fa3ff4cbc0c5e024ba817cf0e6e5b4c1
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-26 13:25:12 -0300

    alter layout user

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -1,7 +1,49 @@
 @extends('layouts.app')
 @include('includes/header')
 
-<div id="container-main">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+<body id="body-container">
+	<div id="container-main">
+		<div class="container">
+			<div class="content">
+				<div class="top-bar">
+					<h1>Contatos</h1>
+					<a href="#"><img src="{{ asset('img/button-add.png') }}" alt="Botão adicionar" id="btnAdd"></a>
+				</div>
+				<div class="aux-bar">
+					<h2>Usuários </h2>
+				</div>
+				<div id="content-table" class="table-responsive">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Nome</th>
+								<th>Usuário</th>
+								<th>Permissão</th>
+							</tr>
+					</thead>  
+						<tbody id="tabela">
+							@foreach($dados as $dado)
+							<tr>
+								<td> {{ $dado->name }} </td>
+								<td> {{ $dado->email }} </td>
+								<td> </td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>  
+</html>
+
+
+<!--
+	<div id="container-main">
     <div class="container">
         <div class="content">
             <div class="top-bar">
@@ -17,13 +59,13 @@
  						</tr>
                    </thead>  
                     <tbody id="tabela">
-                    @foreach($dados as $dado)
+                    	@foreach($dados as $dado)
  						<tr>
  							<td> {{ $dado->name }} </td>
  							<td> {{ $dado->email }} </td>
  							<td> </td>
  						</tr>
- 					@endforeach
+ 						@endforeach
  					</tbody>
  				</table>
  			</div>
@@ -31,3 +73,4 @@
  	</div>
  </div>
 
+-->

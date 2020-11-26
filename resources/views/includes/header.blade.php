4955d26603f519e7f1432548a1dac23dbd3afd45
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-26 20:35:01 -0300

    reorganise header menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -18,9 +18,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('reportShow') }}">Relatório</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('userShow') }}">Usuários</a>
-                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Configurações

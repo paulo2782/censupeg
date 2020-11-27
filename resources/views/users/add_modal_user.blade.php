c72e7e26b24fdcf7864083007299b91aa1df648b
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-27 09:58:07 -0300

    alter add modal user

--- resources/views/users/add_modal_user.blade.php
+++ resources/views/users/add_modal_user.blade.php
@@ -16,7 +16,15 @@
                 <input type="hidden" name="id" value="{{ auth()->user()->id }}">
                 <div class="form-row">       
                     <div class="form-group">
-                        <label for="name">{{ __('Name') }}</label>
+                        <label for="name">Tipo de usuário<span>*</span></label>
+                        <select class="c-select form-control" required>
+                            <option value="" disabled selected hidden>Selecione tipo de usuário</option>
+                            <option value="0">Operador</option>
+                            <option value="1">Administrador</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">{{ __('Name') }}<span>*</span></label>
                         <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Informe seu nome">
                             @error('name')
                                 <span class="invalid-feedback" role="alert">
@@ -25,7 +33,7 @@
                             @enderror
                     </div>
                     <div class="form-group">
-                        <label for="email">{{ __('E-Mail Address') }}</label>
+                        <label for="email">{{ __('E-Mail Address') }}<span>*</span></label>
                         <input type="email"  id="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Informe seu email">
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -34,7 +42,7 @@
                             @enderror
                     </div>
                     <div class="form-group">
-                        <label for="password">{{ __('Password') }}</label>
+                        <label for="password">{{ __('Password') }}<span>*</span></label>
                         <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Informe sua senha">
                         <span toggle="#password" class="fa fa-fw fa-eye field_icon toggle-password"></span>    
                             @error('password')
@@ -44,7 +52,7 @@
                             @enderror
                     </div>
                     <div class="form-group">
-                        <label for="password-confirm">{{ __('Confirm Password') }}</label>
+                        <label for="password-confirm">{{ __('Confirm Password') }}<span>*</span></label>
                         <input type="password" id="password-confirm" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirme sua senha">
                         <span toggle="#password-confirm" class="fa fa-fw fa-eye field_icon toggle-password"></span>
                     </div>

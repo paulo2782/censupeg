6d183bfa84c30062c2881c32fc9b3f689e655827
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-27 12:58:36 -0300

    create modal password

---
+++ resources/views/myaccounts/edit_modal_password.blade.php
@@ -0,0 +1,38 @@
+<div class="modal fade" id="myModalEdit">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Alterar senha <span class="text-5">* Campo Obrigatório</span></h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div id="callback"></div>
+            <form class="form-dialog registerForm" id="updateForm">
+ 
+             <meta name="csrf-token" content="{{ csrf_token() }}">
+                <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+
+                 <div class="form-row">
+                    <div class="form-group col-12">
+                        <label class="text-4" for="name">Senha atual <span>*</span></label>
+                        <input type="password" class="form-control @error('name') is-invalid @enderror" id="edtCourse" name="course" placeholder="Informe o curso" required autocomplete="course" autofocus/>
+                    </div>
+                    <div class="form-group col-12">
+                        <label class="text-4" for="name">Nova senha <span>*</span></label>
+                        <input type="password" class="form-control @error('name') is-invalid @enderror" id="edtCourse" name="course" placeholder="Informe o curso" required autocomplete="course" autofocus/>
+                    </div>
+                    <div class="form-group col-12">
+                        <label class="text-4" for="name">Repetir nova senha <span>*</span></label>
+                        <input type="password" class="form-control @error('name') is-invalid @enderror" id="edtCourse" name="course" placeholder="Informe o curso" required autocomplete="course" autofocus/>
+                    </div>
+
+                </div>
+                <div class="line-horizontal"></div>
+                <input type="hidden" id="idCourse" name="id">
+                <button type="button" id="updateCourse" class="btn btn-primary" data-dismiss=" ">Salvar</button>            
+            </form>
+        </div>
+   </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- resources/views/myaccounts/myaccount.blade.php
+++ resources/views/myaccounts/myaccount.blade.php
@@ -1,1 +1,39 @@
-oi
+@extends('layouts.app')
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+<body id="body-container">
+@include('includes/header')
+<div id="container-main">
+    <div class="container">
+        <div class="content-details">
+            <div class="top-bar-block">
+                <h1>Cursos</h1>
+            </div>
+            <div class="show-details-block">
+                <h2>Graduação</h2>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="#">Blablabla</a>
+                        <div class="pull-right">
+                            <a href="#" class="fa fa-pencil btnEdit" aria-hidden="true" ></a>
+                            <a href="#" class="fa fa-trash btnDelete" aria-hidden="true" ></a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="show-details-block">
+                <h2>Pós-graduação</h2>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="#">Blablabla</a>
+                        <div class="pull-right">
+                            <a href="#" class="fa fa-pencil " aria-hidden="true"></a>
+                            <a href="#" class="fa fa-trash btnDelete" aria-hidden="true"></a>
+                        </div>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="{{ asset('/js/course.js') }}"></script>
+</body>  
+</html>

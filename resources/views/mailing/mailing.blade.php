b1bdc50ff8c9efb329e89a6f1a0b93182257ab80
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-29 18:38:55 -0300

    reorganise mailing

--- resources/views/mailing/mailing.blade.php
+++ resources/views/mailing/mailing.blade.php
@@ -22,14 +22,77 @@
                         <li>Novembro de <span style="font-size:18px">2020</span></li>
                     </ul>
                 </div>
+            </div>
+            <div class="show-details-block">
                 <div id="accordion">
                     <div class="card">
+                        <div class="card-header" id="headingDate">
+                            <div class="text-4 mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    24/11
+                                </button>
+                                <i class="fa fa-file-export"></i>
+                            </div>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingDate" data-parent="#accordion">
+                            <div class="card-body">
+                                <div id="content-table" class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Nome </td>
+                                                <th>Contato data/hora </th>
+                                                <th>Retorno data/hora </th>
+                                                <th>Curso de interesse</th>
+                                                <th>Status</th>
+                                                <th>Ação</th>
+                                            </tr>
+                                        </thead>  
+                                        <tbody id="tabela">
+                                            <tr>
+                                                <td>joao manuel </td>
+                                                <td>29/11/2020 11:10 </td>
+                                                <td>29/11/2020 11:10 </td>
+                                                <td>Educação Física</td>
+                                                <td>Tem interesse</td>
+                                                <td id='toview'>
+                                                    <div class='dropdown'>
+                                                    <img src='img/tres-pontinhos.png' alt='três pontinhos' type='button' id='dropdownImage' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'/>
+                                                    <div class='dropdown-menu' aria-labelledby='dropdownImage'>
+                                                        <a href="#" class='dropdown-item btnToView' id="#">Visualizar</a>
+                                                    </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>joao manuel </td>
+                                                <td>29/11/2020 11:10 </td>
+                                                <td>29/11/2020 11:10 </td>
+                                                <td>Educação Física</td>
+                                                <td>Tem interesse</td>
+                                                <td id='toview'>
+                                                    <div class='dropdown'>
+                                                    <img src='img/tres-pontinhos.png' alt='três pontinhos' type='button' id='dropdownImage' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'/>
+                                                    <div class='dropdown-menu' aria-labelledby='dropdownImage'>
+                                                        <a href="#" class='dropdown-item btnToView' id="#">Visualizar</a>
+                                                    </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="line-horizontal"></div>
+                    <div class="card">
                         <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            24/11
-                            </button>
-                        </h5>
+                            <div class="text-4 mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    25/11
+                                </button>
+                            </div>
                         </div>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                             <div class="card-body">
@@ -67,13 +130,14 @@
                             </div>
                         </div>
                     </div>
+                    <div class="line-horizontal"></div>
                     <div class="card">
                         <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            25/11
-                            </button>
-                        </h5>
+                            <div class="text-4 mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    26/11
+                                </button>
+                            </div>
                         </div>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                             <div class="card-body">
@@ -111,13 +175,14 @@
                             </div>
                         </div>
                     </div>
+                    <div class="line-horizontal"></div>
                     <div class="card">
                         <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            26/11
-                            </button>
-                        </h5>
+                            <div class="text-4 mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    27/11
+                                </button>
+                            </div>
                         </div>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                             <div class="card-body">
@@ -155,13 +220,14 @@
                             </div>
                         </div>
                     </div>
+                    <div class="line-horizontal"></div>
                     <div class="card">
                         <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            27/11
-                            </button>
-                        </h5>
+                            <div class="text-4 mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    28/11
+                                </button>
+                            </div>
                         </div>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                             <div class="card-body">
@@ -199,57 +265,14 @@
                             </div>
                         </div>
                     </div>
+                    <div class="line-horizontal"></div>
                     <div class="card">
                         <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            28/11
-                            </button>
-                        </h5>
-                        </div>
-                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                            <div class="card-body">
-                                <div id="content-table" class="table-responsive">
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th>Nome </td>
-                                                <th>Contato data/hora </th>
-                                                <th>Retorno data/hora </th>
-                                                <th>Curso de interesse</th>
-                                                <th>Status</th>
-                                                <th>Ação</th>
-                                            </tr>
-                                        </thead>  
-                                        <tbody id="tabela">
-                                            <tr>
-                                                <td>joao manuel </td>
-                                                <td>29/11/2020 11:10 </td>
-                                                <td>29/11/2020 11:10 </td>
-                                                <td>Educação Física</td>
-                                                <td>Tem interesse</td>
-                                                <td id='toview'>
-                                                    <div class='dropdown'>
-                                                    <img src='img/tres-pontinhos.png' alt='três pontinhos' type='button' id='dropdownImage' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'/>
-                                                    <div class='dropdown-menu' aria-labelledby='dropdownImage'>
-                                                        <a href="#" class='dropdown-item btnToView' id="#">Visualizar</a>
-                                                    </div>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
+                            <div class="text-4 mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    29/11
+                                </button>
                             </div>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            29/11
-                            </button>
-                        </h5>
                         </div>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                             <div class="card-body">

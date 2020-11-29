0be50d12f0b3aacb2e9c03fb5d489d991592afd6
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-29 08:58:36 -0300

    reorganise form in my accounts

--- resources/views/myaccounts/myaccount.blade.php
+++ resources/views/myaccounts/myaccount.blade.php
@@ -1,48 +1,50 @@
 @extends('layouts.app')
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+@include('includes/header') 
+@include('myaccounts/edit_modal_password')
+
 <body id="body-container">
-@include('includes/header')
-	<div id="container-main">
-		<div class="container">
-			<div class="content-details">
-				<div class="top-bar-block">
-					<h1>Minha conta</h1>
-				</div>
-				<div class="show-details-block">
-					<h2>Informações do usuário
-						<a href="#" class="fa fa-pencil btnEdit" aria-hidden="true" title="#"></a>
-					</h2>
-					<form id="contact-info" method="post">
-						<div class="form-group row">
-							<label class="card-text col-2 col-form-label">Nome:</label>
-							<div class="col-6">
-								<input type="text" class="form-control" readonly="" id="levelCourse" name="level" value="Fulano da silva"/>
-							</div>
+<div id="container-main">
+	<div class="container">
+		<div class="content-details">
+			<div class="top-bar-block">
+				<h1>Minha conta</h1>
+			</div>
+			<div class="show-details-block">
+				<h2>Dados básicos</h2>
+				<form id="contact-info" method="post">
+					<div class="form-row">
+						<div class="form-group col-12">
+							<label class="text-4" for="nameContact">Nome Completo</label>
+							<a href="#" class="fa fa-pencil" aria-hidden="true" id="#"></a>
+							<input type="text" class="form-control" id="nameContact" name="name" value="#" />
 						</div>
-						<div class="form-group row">
-							<label class="card-text col-2 col-form-label">Email:</label>
-							<div class="col-6">
-								<input type="email" class="form-control" readonly="" id="levelCourse" name="level" value="fulanodasilva@email.com"/>
-							</div>
+						<div class="form-group col-12">
+							<label class="text-4" for="emailContact">Email</label>
+							<a href="#" class="fa fa-pencil" aria-hidden="true" id="#"></a>
+							<input type="email" class="form-control" id="emailContact" name="email" value="#" />
+						</div>						
+						<div class="form-group col-12">
+							<label class="text-4" for="stateContact">Tipo de usuário</label>
+							<input type="text" class="form-control" disabled id="stateContact" name="state" value="Operador"/>    
 						</div>
-						<div class="form-group row">
-							<label class="card-text col-2 col-form-label">Senha:</label>
-							<div class="col-6">
-								<input type="password" class="form-control" readonly="" id="levelCourse" name="level" value="***********"/>
-							</div>
+						<div class="form-group col-12">
+							<label class="text-4" for="passwd">Senha</label>
+							<a href="#" class="fa fa-pencil" aria-hidden="true" id="#"></a>
+							<input type="text" class="form-control" readonly="readonly" id="passwd" name="passwd" value="********"/>    
 						</div>
-						<div class="form-group row">
-							<label class="card-text col-2 col-form-label">Tipo de usuário:</label>
-							<div class="col-6">
-								<input type="text" class="form-control" readonly="" id="levelCourse" name="level" value="Operador"/>
-							</div>
-						</div>
-					</form>
-				</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</body>  
-</html>
+<script src="{{ asset('/js/bootstrap-editable.js') }}"></script>
+<script>
+$(function(){
+    $('#username').editable({
+        url: '/post',
+        title: 'Enter username'
+    });
+});
+</script>

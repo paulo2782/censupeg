9396b10319e7892c00f7e53e73a5d682bc11119e
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-11-30 11:06:57 -0300

    resolve layout modal edit modal passwd

--- resources/views/myaccounts/edit_modal_password.blade.php
+++ resources/views/myaccounts/edit_modal_password.blade.php
@@ -3,9 +3,6 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h3 class="modal-title">Alterar senha </h3>
-            </div>
-            <div class="modal-header">            
-                <span class="text-5">Para sua segurança, use uma senha que contenha números, letras maiúsculas, minúsculas e caracteres especiais, e com no mínimo 8 caracteres</span>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -17,23 +14,26 @@
                 <input type="hidden" name="id" id="id_edt_modal">
                  <div class="form-row">
                     <div class="form-group col-12">
-                        <label class="text-4" for="password">Senha atual</label>
-                        <input type="password" class="form-control" id="password" name="old_password" placeholder="Informe a senha atual" required autofocus/>
-                        <span toggle="#password" class="fa fa-fw fa-eye field_icon toggle-password"></span>
+                        <span class="text-5">Para sua segurança, recomenda-se usar uma senha que contenha números, letras maiúsculas, minúsculas e caracteres especiais, e com no mínimo 8 caracteres.</span>
+                    </div>
+                    <div class="form-group col-12">
+                        <label class="text-4" for="old-password">Senha atual</label>
+                        <input type="password" class="form-control" id="old-password" name="old-password" placeholder="Informe a senha atual" required autofocus/>
+                        <span toggle="#old-password" class="fa fa-fw fa-eye field_icon toggle-password"></span>
                     </div>
                     <div class="form-group col-12">
                         <label class="text-4" for="new-password">Nova senha</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Informe a nova senha" required autofocus/>
-                        <span toggle="#password" class="fa fa-fw fa-eye field_icon toggle-password"></span>
+                        <input type="password" class="form-control" id="new-password" name="new-password" placeholder="Informe a nova senha" required autofocus/>
+                        <span toggle="#new-password" class="fa fa-fw fa-eye field_icon toggle-password"></span>
                     </div>
                     <div class="form-group col-12">
                         <label class="text-4" for="confirm-password">Repetir nova senha</label>
-                        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Repetir nova senha" required autofocus/>
-                        <span toggle="#password" class="fa fa-fw fa-eye field_icon toggle-password"></span>
+                        <input type="password" class="form-control" id="confirm-password" name="confirm-password" placeholder="Repetir nova senha" required autofocus/>
+                        <span toggle="#confirm-password" class="fa fa-fw fa-eye field_icon toggle-password"></span>
                     </div>
                 </div>
                 <div class="line-horizontal"></div>
-                <button type="submit" id="#" class="btn btn-success" data-dismiss=" ">Salvar</button>            
+                <button type="submit" id="#" class="btn btn-outline-success" data-dismiss=" ">Salvar</button>            
             </form>
         </div>
    </div>

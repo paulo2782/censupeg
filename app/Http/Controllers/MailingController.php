8991945dcc81465ba64601a1e782a00df2ffeeb2
Paulo Oliveira <paulo2782@gmail.com>
2020-12-02 13:52:18 -0300

    days-months-mailing

--- app/Http/Controllers/MailingController.php
+++ app/Http/Controllers/MailingController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Classes\Monthclass;
 
 class MailingController extends Controller
 {
+
     public function mailingShow(Request $request)
     {
-    	return view('mailing/mailing');
+    	$iMonth = $request->month;
+
+    	$month = new Monthclass();
+
+    	$iDay   = cal_days_in_month(CAL_GREGORIAN, $iMonth, date('Y'));
+    	
+
+    	$strMonth = $month->month($iMonth);
+    			
+    	return view('mailing/mailing',compact('strMonth','iDay','iMonth'));
     }
 
 }
--- resources/views/mailing/mailing.blade.php
+++ resources/views/mailing/mailing.blade.php
@@ -19,19 +19,29 @@
             <div class="show-details-block">
                 <div id="month">
                     <ul>
-                        <li class="prev"><a href="#">&#10094;</a></li>
-                        <li class="next"><a href="#">&#10095;</a></li>
-                        <li>Novembro de <span style="font-size:18px">2020</span></li>
+                        @if($iMonth >= 2)
+                        <li class="prev"><a href="{{ route('mailingShow',intval($iMonth)-1) }}">&#10094;</a></li>
+                        @endif
+
+                        @if($iMonth <= 11)
+                        <li class="next"><a href="{{ route('mailingShow',intval($iMonth)+1) }}">&#10095;</a></li>
+                        @endif
+                        <li>{{ $strMonth }} de <span style="font-size:18px">{{ date('Y') }} </span></li>
                     </ul>
                 </div>
             </div>
+            @for($i = 1 ; $i <= $iDay ; $i++)
             <div class="show-details-block">
                 <div class = "container">
                     <div class = "panel-group">
                         <div class = "panel panel-default">
                             <div class = "panel-heading">
                                 <div class = "panel-title">
-                                    <a data-toggle = "collapse" href = "#test1">01/11
+                                    <a data-toggle = "collapse" href = "#test1">
+                                        @if($i < 10) 0{{ $i }} / @else {{ $i }} / @endif
+                                        @if($iMonth < 10) 0{{ $iMonth }} @else {{ $iMonth }} @endif
+
+
                                         <i class="fas fa-angle-down rotate-icon"></i>
                                     </a>
                                     <span class="export-file text-4">
@@ -66,178 +76,15 @@
                                                     <a href="#"><i class="fas fa-times"></i></a>
                                                 </td>
                                             </tr>
-                                            <tr>
-                                                <td>2</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                         </div>
-                        <div class="dropdown-divider"></div>
-                        <div class = "panel panel-default">
-                            <div class = "panel-heading">
-                                <div class = "panel-title">
-                                    <a data-toggle = "collapse" href = "#test2">02/11
-                                        <i class="fas fa-angle-down rotate-icon"></i>
-                                    </a>
-                                    <span class="export-file text-4">
-                                        <a href="#">Exportar</a>
-                                    </span>
-                                </div>
-                            </div>
-                            <div id = "test2" class="panel-collapse collapse">
-                                <div class="table-responsive">
-                                    <table class="table table-hover mb-0">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Nome</th>
-                                                <th>Contato data/hora</th>
-                                                <th>Retorno data/hora</th>
-                                                <th>Curso de interesse</th>
-                                                <th>Status</th>
-                                                <th>Ação</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>2</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="dropdown-divider"></div>
-                        <div class = "panel panel-default">
-                            <div class = "panel-heading">
-                                <div class = "panel-title">
-                                    <a data-toggle = "collapse" href = "#test3">03/11
-                                        <i class="fas fa-angle-down rotate-icon"></i>
-                                    </a>
-                                    <span class="export-file text-4">
-                                        <a href="#">Exportar</a>
-                                    </span>
-                                </div>
-                            </div>
-                            <div id = "test3" class="panel-collapse collapse">
-                                <div class="table-responsive">
-                                    <table class="table table-hover mb-0">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Nome</th>
-                                                <th>Contato data/hora</th>
-                                                <th>Retorno data/hora</th>
-                                                <th>Curso de interesse</th>
-                                                <th>Status</th>
-                                                <th>Ação</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>2</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</th>
-                                                <td><a href="#">joao manuel</td>
-                                                <td>01/01/2020 11:00</td>
-                                                <td>02/01/2020 12:00</td>
-                                                <td>História</td>
-                                                <td>Tem interesse</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-pen-square"></i></a>
-                                                    <a href="#"><i class="fas fa-times"></i></a>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="dropdown-divider"></div>
                     </div>
                 </div>
             </div>
-            <div class="content">
-                <ul class="pagination"> </ul>
-            </div>
+            @endfor
         </div>
     </div>
 </div>

3cb0c7d2b9f431c928ce54cfe4bf18e4db9bc222
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-12-05 14:15:13 -0300

    add names in inputs

--- resources/views/partners-business/edit_modal_partner.blade.php
+++ resources/views/partners-business/edit_modal_partner.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">Parceria <span class="text-5">* Campo Obrigatório</span></h3>
+                <h3 class="modal-title">Parcerias <span class="text-5">* Campo Obrigatório</span></h3>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>

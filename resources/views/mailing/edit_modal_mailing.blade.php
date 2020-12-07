0d0acb6be49fa4a5d8b862dbc88a420453c697b4
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-12-07 17:45:58 -0300

    add class in mailing

--- resources/views/mailing/edit_modal_mailing.blade.php
+++ resources/views/mailing/edit_modal_mailing.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">Editar Mailing<span class="text-5">* Campo Obrigatório</span></h3>
+                <h3 class="modal-title">Editar Mailing<span class="text-5-title">* Campo Obrigatório</span></h3>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -15,7 +15,7 @@
                 <input type="hidden" name="id" value="{{ auth()->user()->id }}">
                 <div class="form-row">
                     <div class="form-group col-12">
-                        <label class="text-4" for="name">Nome Completo <span>*</span></label>
+                        <label class="text-4" for="name">Nome Completo <span class="text-5">*</span></label>
                         <input type="text" class="form-control @error('name') is-invalid @enderror" id="namxe" name="name" 
                         placeholder= "Informe o nome" value="#" required autocomplete="name" autofocus/>
                         @error('name') {{$message}} @enderror                                     
@@ -26,11 +26,11 @@
                         @error('email') {{$message}} @enderror
                     </div>
                     <div class="form-group col-md-5 col-12">
-                        <label class="text-4" for="phone">Telefone <span>*</span></label>
+                        <label class="text-4" for="phone">Telefone <span class="text-5">*</span></label>
                         <input type="text" class="form-control" id="phone" name="phone" placeholder="(00)0000-0000" value="#" required />    
                     </div>
                     <div class="form-group col-12">
-                        <label class="text-4" >Origem do contato <span>*</span></label>
+                        <label class="text-4" >Origem do contato <span class="text-5">*</span></label>
                         <input type="hidden" id="contactOriginData" value="#">
                         <select class="form-control" id="contactOrigin" name="contact_origin" required>
                             <option value="" disabled selected hidden>Selecione a origem do contato</option>
@@ -55,7 +55,7 @@
                 <div class="dropdown-divider"></div>
                 <div class="form-row">
                     <div class="form-group col-12">
-                        <label class="text-4" for="name">Curso de interesse <span>*</span></label>
+                        <label class="text-4" for="name">Curso de interesse <span class="text-5">*</span></label>
                         <input type="text" class="form-control" id="#" name="#" 
                         placeholder= "Informe o curso de interesse" value="#" required autofocus/>                         
                     </div>
@@ -67,11 +67,11 @@
                         <input type="email" class="form-control" id="email" name="email" placeholder="dd/mm/aaaa" value="#" />
                     </div>
                     <div class="form-group col-md-6 col-12">
-                        <label for="phone">Data retorno <span>*</span></label>
+                        <label for="phone">Data de retorno <span class="text-5">*</span></label>
                         <input type="text" class="form-control" id="phone" name="phone" placeholder="dd/mm/aaaa" value="#" required />    
                     </div>
                     <div class="form-group col-12">
-                        <label for="name">Status <span>*</span></label>
+                        <label for="name">Status <span class="text-5">*</span></label>
                         <input type="text" class="form-control" id="#" name="#" 
                         placeholder= "Selecione status" value="#" required autofocus/>                         
                     </div>
@@ -79,7 +79,7 @@
                 <div class="dropdown-divider"></div>
                 <div class="form-row">
                     <div class="form-group col-12">
-                        <label for="observation">Informações adicionais</label>
+                        <label for="additional_information">Informações adicionais</label>
                         <textarea class="form-control" id="additional_information" name="additional_information">#
                         </textarea>
                     </div>

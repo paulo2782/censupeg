15526cf660289d4e9e0ee001bd2fa62741812773
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-12-14 08:34:20 -0300

    alter name file scss

--- resources/views/dashboards/dashboard.blade.php
+++ resources/views/dashboards/dashboard.blade.php
@@ -8,38 +8,10 @@
         <div class="content-details">
             <div class="top-bar-report">
                 <h1>Dashboard</h1>
-                <div class="period-bar">
-                    <a href="#" class="fa fa-angle-double-left"></a>
-                        Novembro de 2020
-                    <a href="#" class="fa fa-angle-double-right"></a>
-                </div>
+                
             </div>
             <div class="list-reports">
-                <ul class="nav">
-                    <li class="nav-item mr-4">
-                        <a href="#">Contato</a>
-                    </li>
-                    <li class="nav-item mr-4">
-                        <a href="#">Ligações</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#">Curso</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="contact-reports">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 col-12 contact-reports-charts">
-                            <h3>Escolaridade</h3>
-                            <div id="escolaridade"></div>
-                        </div>
-                        <div class="col-md-6 col-12 contact-reports-charts">
-                            <h3>Origem dos contatos</h3>
-                            <div id="origem-contato"></div>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>

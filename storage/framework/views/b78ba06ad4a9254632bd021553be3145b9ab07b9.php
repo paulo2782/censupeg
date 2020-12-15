65572348a63fe453906157199b3a979d13ad4162
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-12-15 20:41:06 -0300

    remove cache

--- storage/framework/views/b78ba06ad4a9254632bd021553be3145b9ab07b9.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-	<div class="copyright">
-	    © <?php echo e(date('Y')); ?> - Todos os Direitos Reservados
-	</div>
-</footer>  
-</div>
- <?php /**PATH C:\censupeg\resources\views/includes/footer.blade.php ENDPATH**/ ?>

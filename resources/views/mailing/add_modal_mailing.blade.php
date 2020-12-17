d7d1beb3b4d2947ba226e14fe2b43af4108f7478
psotorressistemas <psotorressistemas@gmail.com>
2020-12-17 02:01:08 -0300

    autocomplete-fields

--- resources/views/mailing/add_modal_mailing.blade.php
+++ resources/views/mailing/add_modal_mailing.blade.php
@@ -33,8 +33,8 @@
                         @error('email') {{$message}} @enderror
                     </div>
                     <div class="form-group col-md-5 col-12">
-                        <label for="phone">Telefone <span class="text-5">*</span></label>
-                        <input type="text" class="form-control" id="phone" name="phone" placeholder="(00)0000-0000" value="" readonly />    
+                        <label for="phone">Telefone <span class="text-5"></span></label>
+                        <input type="text" class="form-control" id="phone" name="phone" placeholder="(00)0000-0000" value="" readonly required />    
                     </div>
                     <!--CURSO -->
                     <div class="form-group col-12">
